8a7d61d370a361d444412f168048f3df7c3906f0
vishal9501 <95237922+vishal9501@users.noreply.github.com>
2022-09-11 02:33:35 +0530

    Add files via upload

---
+++ Music Player/js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+  {
+    name: "Kesariya",
+    artist: "Arjit Singh",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Saathiya",
+    artist: "Nikhil D souza",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "meri maa ke barabar koi nahi",
+    artist: "jubin Nautiyal",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Bhor Bhai Bin chad",
+    artist: "various",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Unstoppable",
+    artist: "The Score",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "teri Galliyon",
+    artist: "Jubin Nautiyal,Meet bros",
+    img: "music-6",
+    src: "music-6"
+  },
+];
